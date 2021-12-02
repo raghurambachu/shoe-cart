c651538da0f66907fc60a7903a7e1b09e93d655c
Raghuram Bachu <raghurambachu@Betafluxs-MacBook-Pro.local>
2021-12-02 22:45:12 +0530

    Generate merchandise data

--- src/data.ts
+++ src/data.ts
@@ -17,6 +17,357 @@ const merchandiseData = [
         alt: "balenciaga-image3",
       },
     ],
+    band: "band-type1",
+    price: 790,
+    category: ["sneakers", "shoe-accessories"],
+    availableSizes: [39, 40, 41, 42, 43],
+    compositionsAvailable: [
+      {
+        material: "Polyamide",
+        percentage: 100,
+      },
+      {
+        material: "Rubber",
+        percentage: 100,
+      },
+      {
+        material: "Synthetic",
+        percentage: 90,
+      },
+    ],
+    description:
+      "We understand that some sneakers are greater than others and worn once in a blue moon. So for those extra special occasions why not try this collaborative effort from Gyakusou and Nike with these.It's all in the footwear. And these logo-print Speed 2.0 sneakers from Balenciaga tick every box. They'll be the focal point of your whole look.",
+  },
+  {
+    sku: "1000",
+    feature: "High Speed Sneakers",
+    name: "Balenciaga",
+    images: [
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30900316_1000.jpg",
+        alt: "balenciaga-image1",
+      },
+      {
+        url: "http://www.dailyblogvlog.com/images/soni/balenciaga%20shoes%20sock-211ifh.jpg",
+        alt: "balenciaga-image2",
+      },
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30897577_1000.jpg",
+        alt: "balenciaga-image3",
+      },
+    ],
+    band: "orange",
+    price: 790,
+    category: ["sneakers", "shoe-accessories"],
+    availableSizes: [39, 40, 41, 42, 43],
+    compositionsAvailable: [
+      {
+        material: "Polyamide",
+        percentage: 100,
+      },
+      {
+        material: "Rubber",
+        percentage: 100,
+      },
+      {
+        material: "Synthetic",
+        percentage: 90,
+      },
+    ],
+    description:
+      "We understand that some sneakers are greater than others and worn once in a blue moon. So for those extra special occasions why not try this collaborative effort from Gyakusou and Nike with these.It's all in the footwear. And these logo-print Speed 2.0 sneakers from Balenciaga tick every box. They'll be the focal point of your whole look.",
+  },
+  {
+    sku: "1000",
+    feature: "High Speed Sneakers",
+    name: "Balenciaga",
+    images: [
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30900316_1000.jpg",
+        alt: "balenciaga-image1",
+      },
+      {
+        url: "http://www.dailyblogvlog.com/images/soni/balenciaga%20shoes%20sock-211ifh.jpg",
+        alt: "balenciaga-image2",
+      },
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30897577_1000.jpg",
+        alt: "balenciaga-image3",
+      },
+    ],
+    band: "orange",
+    price: 790,
+    category: ["sneakers", "shoe-accessories"],
+    availableSizes: [39, 40, 41, 42, 43],
+    compositionsAvailable: [
+      {
+        material: "Polyamide",
+        percentage: 100,
+      },
+      {
+        material: "Rubber",
+        percentage: 100,
+      },
+      {
+        material: "Synthetic",
+        percentage: 90,
+      },
+    ],
+    description:
+      "We understand that some sneakers are greater than others and worn once in a blue moon. So for those extra special occasions why not try this collaborative effort from Gyakusou and Nike with these.It's all in the footwear. And these logo-print Speed 2.0 sneakers from Balenciaga tick every box. They'll be the focal point of your whole look.",
+  },
+  {
+    sku: "1000",
+    feature: "High Speed Sneakers",
+    name: "Balenciaga",
+    images: [
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30900316_1000.jpg",
+        alt: "balenciaga-image1",
+      },
+      {
+        url: "http://www.dailyblogvlog.com/images/soni/balenciaga%20shoes%20sock-211ifh.jpg",
+        alt: "balenciaga-image2",
+      },
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30897577_1000.jpg",
+        alt: "balenciaga-image3",
+      },
+    ],
+    band: "orange",
+    price: 790,
+    category: ["sneakers", "shoe-accessories"],
+    availableSizes: [39, 40, 41, 42, 43],
+    compositionsAvailable: [
+      {
+        material: "Polyamide",
+        percentage: 100,
+      },
+      {
+        material: "Rubber",
+        percentage: 100,
+      },
+      {
+        material: "Synthetic",
+        percentage: 90,
+      },
+    ],
+    description:
+      "We understand that some sneakers are greater than others and worn once in a blue moon. So for those extra special occasions why not try this collaborative effort from Gyakusou and Nike with these.It's all in the footwear. And these logo-print Speed 2.0 sneakers from Balenciaga tick every box. They'll be the focal point of your whole look.",
+  },
+  {
+    sku: "1000",
+    feature: "High Speed Sneakers",
+    name: "Balenciaga",
+    images: [
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30900316_1000.jpg",
+        alt: "balenciaga-image1",
+      },
+      {
+        url: "http://www.dailyblogvlog.com/images/soni/balenciaga%20shoes%20sock-211ifh.jpg",
+        alt: "balenciaga-image2",
+      },
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30897577_1000.jpg",
+        alt: "balenciaga-image3",
+      },
+    ],
+    band: "orange",
+    price: 790,
+    category: ["sneakers", "shoe-accessories"],
+    availableSizes: [39, 40, 41, 42, 43],
+    compositionsAvailable: [
+      {
+        material: "Polyamide",
+        percentage: 100,
+      },
+      {
+        material: "Rubber",
+        percentage: 100,
+      },
+      {
+        material: "Synthetic",
+        percentage: 90,
+      },
+    ],
+    description:
+      "We understand that some sneakers are greater than others and worn once in a blue moon. So for those extra special occasions why not try this collaborative effort from Gyakusou and Nike with these.It's all in the footwear. And these logo-print Speed 2.0 sneakers from Balenciaga tick every box. They'll be the focal point of your whole look.",
+  },
+  {
+    sku: "1000",
+    feature: "High Speed Sneakers",
+    name: "Balenciaga",
+    images: [
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30900316_1000.jpg",
+        alt: "balenciaga-image1",
+      },
+      {
+        url: "http://www.dailyblogvlog.com/images/soni/balenciaga%20shoes%20sock-211ifh.jpg",
+        alt: "balenciaga-image2",
+      },
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30897577_1000.jpg",
+        alt: "balenciaga-image3",
+      },
+    ],
+    band: "orange",
+    price: 790,
+    category: ["sneakers", "shoe-accessories"],
+    availableSizes: [39, 40, 41, 42, 43],
+    compositionsAvailable: [
+      {
+        material: "Polyamide",
+        percentage: 100,
+      },
+      {
+        material: "Rubber",
+        percentage: 100,
+      },
+      {
+        material: "Synthetic",
+        percentage: 90,
+      },
+    ],
+    description:
+      "We understand that some sneakers are greater than others and worn once in a blue moon. So for those extra special occasions why not try this collaborative effort from Gyakusou and Nike with these.It's all in the footwear. And these logo-print Speed 2.0 sneakers from Balenciaga tick every box. They'll be the focal point of your whole look.",
+  },
+  {
+    sku: "1000",
+    feature: "High Speed Sneakers",
+    name: "Balenciaga",
+    images: [
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30900316_1000.jpg",
+        alt: "balenciaga-image1",
+      },
+      {
+        url: "http://www.dailyblogvlog.com/images/soni/balenciaga%20shoes%20sock-211ifh.jpg",
+        alt: "balenciaga-image2",
+      },
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30897577_1000.jpg",
+        alt: "balenciaga-image3",
+      },
+    ],
+    band: "orange",
+    price: 790,
+    category: ["sneakers", "shoe-accessories"],
+    availableSizes: [39, 40, 41, 42, 43],
+    compositionsAvailable: [
+      {
+        material: "Polyamide",
+        percentage: 100,
+      },
+      {
+        material: "Rubber",
+        percentage: 100,
+      },
+      {
+        material: "Synthetic",
+        percentage: 90,
+      },
+    ],
+    description:
+      "We understand that some sneakers are greater than others and worn once in a blue moon. So for those extra special occasions why not try this collaborative effort from Gyakusou and Nike with these.It's all in the footwear. And these logo-print Speed 2.0 sneakers from Balenciaga tick every box. They'll be the focal point of your whole look.",
+  },
+  {
+    sku: "1000",
+    feature: "High Speed Sneakers",
+    name: "Balenciaga",
+    images: [
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30900316_1000.jpg",
+        alt: "balenciaga-image1",
+      },
+      {
+        url: "http://www.dailyblogvlog.com/images/soni/balenciaga%20shoes%20sock-211ifh.jpg",
+        alt: "balenciaga-image2",
+      },
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30897577_1000.jpg",
+        alt: "balenciaga-image3",
+      },
+    ],
+    band: "orange",
+    price: 790,
+    category: ["sneakers", "shoe-accessories"],
+    availableSizes: [39, 40, 41, 42, 43],
+    compositionsAvailable: [
+      {
+        material: "Polyamide",
+        percentage: 100,
+      },
+      {
+        material: "Rubber",
+        percentage: 100,
+      },
+      {
+        material: "Synthetic",
+        percentage: 90,
+      },
+    ],
+    description:
+      "We understand that some sneakers are greater than others and worn once in a blue moon. So for those extra special occasions why not try this collaborative effort from Gyakusou and Nike with these.It's all in the footwear. And these logo-print Speed 2.0 sneakers from Balenciaga tick every box. They'll be the focal point of your whole look.",
+  },
+  {
+    sku: "1000",
+    feature: "High Speed Sneakers",
+    name: "Balenciaga",
+    images: [
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30900316_1000.jpg",
+        alt: "balenciaga-image1",
+      },
+      {
+        url: "http://www.dailyblogvlog.com/images/soni/balenciaga%20shoes%20sock-211ifh.jpg",
+        alt: "balenciaga-image2",
+      },
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30897577_1000.jpg",
+        alt: "balenciaga-image3",
+      },
+    ],
+    band: "orange",
+    price: 790,
+    category: ["sneakers", "shoe-accessories"],
+    availableSizes: [39, 40, 41, 42, 43],
+    compositionsAvailable: [
+      {
+        material: "Polyamide",
+        percentage: 100,
+      },
+      {
+        material: "Rubber",
+        percentage: 100,
+      },
+      {
+        material: "Synthetic",
+        percentage: 90,
+      },
+    ],
+    description:
+      "We understand that some sneakers are greater than others and worn once in a blue moon. So for those extra special occasions why not try this collaborative effort from Gyakusou and Nike with these.It's all in the footwear. And these logo-print Speed 2.0 sneakers from Balenciaga tick every box. They'll be the focal point of your whole look.",
+  },
+  {
+    sku: "1000",
+    feature: "High Speed Sneakers",
+    name: "Balenciaga",
+    images: [
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30900316_1000.jpg",
+        alt: "balenciaga-image1",
+      },
+      {
+        url: "http://www.dailyblogvlog.com/images/soni/balenciaga%20shoes%20sock-211ifh.jpg",
+        alt: "balenciaga-image2",
+      },
+      {
+        url: "https://cdn-images.farfetch-contents.com/16/11/50/14/16115014_30897577_1000.jpg",
+        alt: "balenciaga-image3",
+      },
+    ],
     band: "orange",
     price: 790,
     category: ["sneakers", "shoe-accessories"],
