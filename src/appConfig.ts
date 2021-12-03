69bcba54b5069917c9db461b605c8a1881e1b4c6
Raghuram Bachu <raghurambachu@Betafluxs-MacBook-Pro.local>
2021-12-04 01:28:16 +0530

    Add size filter cells

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,5 +1,6 @@
 const appConfig = {
   list: {
+    //same structure things are kept under list
     sortBy: [
       { label: "Price", value: "price" },
       { label: "Order", value: "order" },
@@ -11,6 +12,7 @@ const appConfig = {
       { label: "Shoe Accessories", value: "shoe-accessories" },
     ],
   },
+  availableSizes: [35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49],
   uiConfigs: {
     sidebarPadding: "3rem",
   },
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,6 +9,7 @@ import CheckboxGroup from "./CheckboxGroup";
 const {
   uiConfigs: { sidebarPadding },
   list: { categories },
+  availableSizes,
 } = appConfig;
 
 const AccordionContent = styled.div`
@@ -22,6 +23,38 @@ const SidebarWrapper = styled.div``;
 const Categories = styled.div`
   .accordion-title {
     font-weight: 500;
+  }
+`;
+
+const SizeFilterWrapper = styled.div`
+  padding: 2.5rem ${sidebarPadding};
+  .size-filter-title {
+    font-size: 1.4rem;
+    font-weight: 500;
+    margin-bottom: 2.5rem;
+  }
+`;
+
+const AvailableSizes = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 1rem;
+`;
+
+const Cell = styled.div`
+  width: 3.8rem;
+  height: 3.8rem;
+  display: flex;
+  cursor: pointer;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.4rem;
+  background: var(--hover-grey);
+  border-radius: 0.4rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    transform: scale(1.07);
   }
 `;
 
@@ -71,6 +104,14 @@ const Sidebar = () => {
           }}
         />
       </Categories>
+      <SizeFilterWrapper>
+        <h3 className="size-filter-title">Size</h3>
+        <AvailableSizes>
+          {availableSizes.map((size) => {
+            return <Cell key={size}>{size}</Cell>;
+          })}
+        </AvailableSizes>
+      </SizeFilterWrapper>
     </SidebarWrapper>
   );
 };
